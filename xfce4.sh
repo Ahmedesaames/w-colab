eb47ca997b63138e4606cabd48680bfa35e95b10
Ahmedesaames <84388451+Ahmedesaames@users.noreply.github.com>
2022-12-23 09:42:38 -0800

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,6 +1,5 @@
 echo "Download ngrok"
 wget https://raw.githubusercontent.com/Ahmedesaames/w-colab/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
-echo Downloading File From akuh.net
 echo "===================================="
 echo "Wait 2 Minutes"
 echo "This is only for education"
@@ -25,7 +24,4 @@ sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "More  Free VPS akuh.net"
-echo "Don't close this tab RDP runs 12 hours"
-echo "Keep support akuh.net thank you"
-echo "===================================="
+sleep 46800
